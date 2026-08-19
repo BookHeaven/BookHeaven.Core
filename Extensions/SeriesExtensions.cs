4bf42e536e2cad7b6c9c539308a646fc4230c0dc
heasheartfire <gagi1995@gmail.com>
2026-08-19 13:04:27 +0200

    feat(SeriesExtensions): add UpdateFrom method for Series

---
+++ Extensions/SeriesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using BookHeaven.Domain.Entities;
+
+namespace BookHeaven.Domain.Extensions;
+
+public static class SeriesExtensions
+{
+    extension(Series series)
+    {
+        public void UpdateFrom(Series updatedSeries)
+        {
+            series.Name = updatedSeries.Name;
+        }
+    }
+}
