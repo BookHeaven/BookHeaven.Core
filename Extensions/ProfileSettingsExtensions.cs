5ba6cf587e3b13cf55db4fc0faddb9bfdf5ad465
heasheartfire <gagi1995@gmail.com>
2026-08-19 13:04:17 +0200

    feat(ProfileSettingsExtensions): add UpdateFrom method for ProfileSettings

---
+++ Extensions/ProfileSettingsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BookHeaven.Domain.Entities;
+
+namespace BookHeaven.Domain.Extensions;
+
+public static class ProfileSettingsExtensions
+{
+    extension(ProfileSettings settings)
+    {
+        public void UpdateFrom(ProfileSettings updatedSettings)
+        {
+            settings.FontSize = updatedSettings.FontSize;
+            settings.LineHeight = updatedSettings.LineHeight;
+            settings.LetterSpacing = updatedSettings.LetterSpacing;
+            settings.WordSpacing = updatedSettings.WordSpacing;
+            settings.ParagraphSpacing = updatedSettings.ParagraphSpacing;
+            settings.TextIndent = updatedSettings.TextIndent;
+            settings.HorizontalMargin = updatedSettings.HorizontalMargin;
+            settings.VerticalMargin = updatedSettings.VerticalMargin;
+            settings.PageGap = updatedSettings.PageGap;
+            settings.SelectedLayout = updatedSettings.SelectedLayout;
+            settings.SelectedFont = updatedSettings.SelectedFont;
+        }
+    }
+}
