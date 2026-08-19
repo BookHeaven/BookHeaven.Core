ea1427541dff1069d04e10cff9d4037945e13318
heasheartfire <gagi1995@gmail.com>
2026-08-19 13:08:03 +0200

    fix(UpdateSeries): fetch and update item from context instead of trying to save the request object

--- Features/BookSeries/UpdateSeries.cs
+++ Features/BookSeries/UpdateSeries.cs
@@ -1,17 +1,24 @@
 ﻿using BookHeaven.Domain.Entities;
+using BookHeaven.Domain.Extensions;
 
 namespace BookHeaven.Domain.Features.BookSeries;
 
 public static class UpdateSeries {
-    public sealed record Command(Series Series) : ICommand<Series>;
+    public sealed record Command(Series Series) : ICommand;
 
-    internal class Handler(IDbContextFactory<DatabaseContext> dbContextFactory) : ICommandHandler<Command, Series>
+    internal class Handler(IDbContextFactory<DatabaseContext> dbContextFactory) : ICommandHandler<Command>
     {
-        public async ValueTask<Result<Series>> Handle(Command request, CancellationToken cancellationToken)
+        public async ValueTask<Result> Handle(Command request, CancellationToken cancellationToken)
         {
             await using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
-
-            context.Series.Update(request.Series);
+            
+            var existingSeries = await context.Series.FirstOrDefaultAsync(s => s.SeriesId == request.Series.SeriesId, cancellationToken);
+            if (existingSeries is null)
+            {
+                return new Error("Series not found");
+            }
+            
+            existingSeries.UpdateFrom(request.Series);
 
             try
             {
@@ -21,8 +28,8 @@ public static class UpdateSeries {
             {
                 return new Error("An error occurred while updating the series");
             }
-
-            return request.Series;
+            
+            return Result.Success();
         }
     }
 }
