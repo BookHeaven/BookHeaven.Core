f7259b6acc40aef3d8b1a089133318d984089d4b
heasheartfire <gagi1995@gmail.com>
2026-08-19 13:03:35 +0200

    feat(BookProgressExtensions): add UpdateFrom method

--- Extensions/BookProgressExtensions.cs
+++ Extensions/BookProgressExtensions.cs
@@ -4,14 +4,23 @@ namespace BookHeaven.Domain.Extensions;
 
 public static class BookProgressExtensions
 {
-    public static string ElapsedTimeFormatted(this BookProgress progress)
-    {
-        return $"{(int)progress.ElapsedTime.TotalHours} h {progress.ElapsedTime.Minutes:00} m";
     extension(BookProgress progress)
     {
         public string ElapsedTimeFormatted()
         {
             return $"{(int)progress.ElapsedTime.TotalHours} h {progress.ElapsedTime.Minutes:00} m";
+        }
+
+        public void UpdateFrom(BookProgress updatedProgress)
+        {
+            progress.Chapter = updatedProgress.Chapter;
+            progress.ChapterProgress = updatedProgress.ChapterProgress;
+            progress.Progress = updatedProgress.Progress;
+            progress.StartDate = updatedProgress.StartDate;
+            progress.EndDate = updatedProgress.EndDate;
+            progress.LastRead = updatedProgress.LastRead;
+            progress.ElapsedTime = updatedProgress.ElapsedTime;
+            progress.BookWordCount = updatedProgress.BookWordCount;
         }
     }
 }
