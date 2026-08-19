0d13ced22b253968125862ff38ed70ed5e406fef
heasheartfire <gagi1995@gmail.com>
2026-08-19 13:04:03 +0200

    feat(AuthorExtensions): add UpdateFrom method

---
+++ Extensions/AuthorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BookHeaven.Domain.Entities;
+
+namespace BookHeaven.Domain.Extensions;
+
+public static class AuthorExtensions
+{
+    extension(Author author)
+    {
+        public void UpdateFrom(Author updatedAuthor)
+        {
+            author.Name = updatedAuthor.Name;
+            author.Biography = updatedAuthor.Biography;
+        }
+    }
+}
