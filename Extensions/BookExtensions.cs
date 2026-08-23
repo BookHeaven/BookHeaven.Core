f8ce2d914a4618427bbaf77d7b1fe0130740e665
heasheartfire <gagi1995@gmail.com>
2026-08-23 21:00:51 +0200

    refactor(BookExtensions): fix format

--- Extensions/BookExtensions.cs
+++ Extensions/BookExtensions.cs
@@ -29,16 +29,16 @@ public static class BookExtensions
 
 	    public string EbookUrl() => "/books/" + book.BookId + book.Format.GetExtension();
 
-	    public string CoverUrl(bool appendVersion = false)
-	    {
-		    var coverExists = File.Exists(book.CoverPath());
-		    if (!coverExists) return "/img/no-cover.jpg";
-	    
-		    var url = "/covers/" + book.BookId + ".jpg";
-		    if(!appendVersion) return url;
-	    
-		    return url + "?v=" + new FileInfo(book.CoverPath()).LastWriteTimeUtc.Ticks;
-	    }
+		public string CoverUrl(bool appendVersion = false)
+		{
+			var coverExists = File.Exists(book.CoverPath());
+			if (!coverExists) return "/img/no-cover.jpg";
+
+			var url = "/covers/" + book.BookId + ".jpg";
+			if(!appendVersion) return url;
+
+			return url + "?v=" + new FileInfo(book.CoverPath()).LastWriteTimeUtc.Ticks;
+		}
 
 	    public string EbookPath() => Path.Combine(CoreGlobals.BooksPath, $"{book.BookId}{book.Format.GetExtension()}");
 	    public string CoverPath() => Path.Combine(CoreGlobals.CoversPath, $"{book.BookId}.jpg");
