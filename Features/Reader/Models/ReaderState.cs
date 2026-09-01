1fbabff284fa3a5a21228eda314b738bde7ba383
heasheartfire <gagi1995@gmail.com>
2026-09-01 13:54:59 +0200

    feat(ReaderState): enhance state management with encapsulated properties and methods

--- Features/Reader/Models/ReaderState.cs
+++ Features/Reader/Models/ReaderState.cs
@@ -1,30 +1,56 @@
+using BookHeaven.Core.Entities;
 using BookHeaven.EbookManager.Entities;
 
 namespace BookHeaven.Core.Features.Reader.Models;
 
 public sealed class ReaderState
 {
-    public Ebook? Ebook { get; set; }
-    public int ChapterNumber { get; set; }
-    public int PageNumber { get; set; }
-    public int TotalPages { get; set; }
-    public int TotalPagesPrev { get; set; }
-    public int TotalPagesNext { get; set; }
-    public IReadOnlyList<Stylesheet> Styles { get; set; } = [];
+    public Ebook? Ebook { get; private set; }
+    public int ChapterNumber { get; private set; }
+    public int PageNumber { get; private set; }
+    public int PageBookNumber { get; private set; }
+    public int TotalPages { get; private set; }
+    private int BookPages { get; set; }
+    public int[] PagesPerChapter { get; private set; } = [];
+    public IReadOnlyList<Stylesheet> Styles { get; private set; } = [];
     public DateTimeOffset EntryTime { get; set; }
+    public DateTimeOffset SuspendStartTime { get; set; }
+    public TimeSpan TotalSuspendedTime { get; set; }
     
-    public Chapter? CurrentChapter => Ebook?.Content.Chapters.ElementAtOrDefault(ChapterNumber);
-    public Chapter? PreviousChapter => Ebook?.Content.Chapters.ElementAtOrDefault(ChapterNumber - 1);
-    public Chapter? NextChapter => Ebook?.Content.Chapters.ElementAtOrDefault(ChapterNumber + 1);
-    private TocEntry? CurrentTocEntry => Ebook?.Content.GetChapterFromTableOfContents(CurrentChapter?.Identifier);
-    public string ChapterTitle => !string.IsNullOrEmpty(CurrentChapter?.Title) 
-        ? CurrentChapter!.Title
-        : CurrentTocEntry?.Title ?? string.Empty;
-    public int TotalChapters => Ebook?.Content.Chapters.Count ?? 0;
-    public int TotalWeight => Ebook?.Content.Chapters.Sum(c => c.Weight) ?? 0;
+    public Chapter? CurrentChapter { get; private set; }
+    private TocEntry? CurrentTocEntry { get; set; }
+    public string ChapterTitle { get; private set; } = string.Empty;
+    public int TotalChapters { get; private set; }
     
-    public decimal ProgressAsPercentage => Ebook is not null && CurrentChapter is not null && TotalWeight != 0
-        ? (Ebook.Content.GetTotalWeight(ChapterNumber) + CurrentChapter.WeightPerPage(TotalPages) * PageNumber) 
-            / (decimal)TotalWeight * 100
+    public decimal ProgressAsPercentage => Ebook is not null && BookPages > 0
+        ? ((decimal)PageBookNumber / BookPages) * 100
         : 0;
+    
+    public void SetEbook(Ebook ebook)
+    {
+        Ebook = ebook;
+        Styles = Ebook.Content.Stylesheets;
+        TotalChapters = Ebook!.Content.Chapters.Count;
+    }
+    
+    public void SetChapterAndPage(int chapterNumber, int pageNumber)
+    {
+        ChapterNumber = chapterNumber;
+        TotalPages = PagesPerChapter.ElementAtOrDefault(chapterNumber);
+        CurrentChapter = Ebook?.Content.Chapters.ElementAtOrDefault(ChapterNumber);
+        CurrentTocEntry = Ebook?.Content.GetChapterFromTableOfContents(CurrentChapter?.Identifier);
+        ChapterTitle = !string.IsNullOrEmpty(CurrentTocEntry?.Title)
+            ? CurrentTocEntry.Title
+            : CurrentChapter?.Title ?? string.Empty;
+        
+        PageNumber = pageNumber;
+        PageBookNumber = PagesPerChapter.Take(ChapterNumber).Sum() + PageNumber;
+    }
+    
+    public void SetPagesPerChapter(int[] pagesPerChapter)
+    {
+        PagesPerChapter = pagesPerChapter;
+        BookPages = pagesPerChapter.Sum();
+        SetChapterAndPage(ChapterNumber, PageNumber);
+    }
 }
